ee585a653726ee76277c3b79b9a9f33983f9f9c8
Bolakunmi Oluwayemi <52560352+bolakunmi@users.noreply.github.com>
2022-07-01 12:48:23 +0100

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,8 @@
+const move_up = document.querySelector(".move_up");
+move_up.addEventListener("click", function () {
+  window.scrollTo(0, 0);
+});
+
+// const move_up = function() {
+//     window.scrollTo(0,0);
+// }
